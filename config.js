bfea2ef671dfe7e329a9f4cf7b52c1162255b870
velcroapple <avishavipul@gmail.com>
2026-02-01 20:59:21 +0530

    Modify celebration emojis and animation parameters
    Updated celebration emojis and adjusted animation settings.

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 💖",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "💖🤗💝💋❤️💕"  // These will bounce around
+        emojis: "💕"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -68,9 +68,9 @@ const CONFIG = {
     // Adjust these if you want faster/slower animations
     animations: {
         floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
-        floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
+        floatDistance: "70px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
+        heartExplosionSize: 1.3        // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
